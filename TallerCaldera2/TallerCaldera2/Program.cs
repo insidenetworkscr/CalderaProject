1fdfa1126dcf79bd7ee306b77d59cc6f0689fdb1
Rogerfps <rpereira40667@ufide.ac.cr>
2025-11-05 17:22:39 -0600

    migrations

--- TallerCaldera2/TallerCaldera2/Program.cs
+++ TallerCaldera2/TallerCaldera2/Program.cs
@@ -1,15 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TallerCaldera.Models;
+using TallerCaldera2.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,8 +26,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
