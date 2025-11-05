033699b8acc2e3f28a7bfc444a67e2f443e0ff1f
Rogerfps <rpereira40667@ufide.ac.cr>
2025-11-05 16:29:57 -0600

    borrar

--- TallerCaldera/TallerCaldera/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace TallerCaldera.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        
-    }
-}
--- TallerCaldera/TallerCaldera/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TallerCaldera.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
