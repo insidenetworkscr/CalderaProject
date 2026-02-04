961e0a171b678e0588e5b36506db5a7e3fc17f7d
Rogerfps <rpereira40667@ufide.ac.cr>
2026-02-03 22:39:38 -0600

    Marcas ajuste

--- TallerCaldera2/TallerCaldera2/obj/Debug/net8.0/TallerCaldera2.AssemblyInfo.cs
+++ TallerCaldera2/TallerCaldera2/obj/Debug/net8.0/TallerCaldera2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TallerCaldera2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa5d978f8151acfb39ca86e552f608061b1a0372")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ae251c5c2a95a8a695c8f121e10de18d628ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TallerCaldera2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TallerCaldera2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
